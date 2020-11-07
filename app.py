2a79068a73bb01ba16d3e92e9bdd35b4d15e2593
llwyd <tom@llwyd.io>
2020-11-07 16:43:38 +0000

    cookies to identify client working

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template, flash, request, redirect, url_for, make_response
+from PIL import Image, ImageCms
+from werkzeug.utils import secure_filename
+import uuid
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+        client_id = request.cookies.get('client_id')
+        if client_id is None:
+            # create new uuid
+            client_id = str(uuid.uuid4())
+            print("New User")
+            resp = make_response(render_template('index.html',client_id=client_id))
+            resp.set_cookie('client_id',client_id)
+            return resp
+        else:
+            return render_template('index.html',client_id=client_id)
 
