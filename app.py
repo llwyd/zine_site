7fada9bcd4970dd9e630c67afdfe4b7469facfa4
llwyd <tom@llwyd.io>
2020-11-07 18:37:53 +0000

    file uploading, saving with uuid now working

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, flash, request, redirect, url_for, mak
 from PIL import Image, ImageCms
 from werkzeug.utils import secure_filename
 import uuid
+import os
+from pathlib import Path
 
 app = Flask(__name__)
 
-
+app.config['CLIENT_ID'] = ''
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
-def hello_world():
+def main_page():
         client_id = request.cookies.get('client_id')
         if client_id is None:
             # create new uuid
@@ -20,3 +24,25 @@ def hello_world():
         else:
             return render_template('index.html',client_id=client_id)
 
+@app.route('/',methods=['POST'])
+def upload_image():
+    # Retrieve client id
+    client_id = request.cookies.get('client_id')
+    if client_id is None:
+        # should never reach here
+        return redirect(url_for('critical_failure'))
+    print(client_id)
+    
+    # Process incoming file
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = Path(filename).suffix
+        print(file_ext)
+        uploaded_file.save(app.config['UPLOAD_PATH'] + '/' + client_id + str('_s') + file_ext)
+
+    return redirect(url_for('main_page'))
+
+@app.route('/assert')
+def critical_failure():
+    return 'CRITICAL FAILURE :('
